2516e55c6e32d088d34069063d0f6711324a4f76
ianiketkumar7 <aniketkumar.nikeman@gmail.com>
2025-04-27 10:33:09 +0530

    comit

--- JavaBasics.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class JavaBasics{
-    public static void main(String args[]) {
-        int num = 5;
-        String check = (num % 2 == 0) ? "EVEN" : "ODD";
-        System.out.println(check);
-    }
-}
--- PassOrFailForStudent.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class PassOrFailForStudent{
-    public static void main(String args[]){
-        int marks = 7;
-        String result = (marks >= 33) ? "PASS" : "FAIL";
-        System.out.println(result);
-    }
-}
